f6b317068bb2772101784903c1563fb73dd51e78
unknown <aditya.p@medkart.in>
2023-02-01 15:32:04 +0530

    created expandables cards under schemes tab

--- src/Schemes.js
+++ src/Schemes.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import Expandablecards from "./Expandablecards";
 
 export default function Schemes() {
   return (
-    <div>Schemes</div>
-  )
+    <>
+      <div>
+        <div>schemesdfvcdsfd</div>
+        <div style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
+          <Expandablecards />
+          <Expandablecards />
+          <Expandablecards />
+          <Expandablecards />
+          <Expandablecards />
+          <Expandablecards />
+        </div>
+      </div>
+    </>
+  );
 }
