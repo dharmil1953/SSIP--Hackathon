e9c64ddd6619d31a037f1da5303b98b3b9e8068b
unknown <aditya.p@medkart.in>
2023-02-03 17:53:25 +0530

    added  test api laravel connection

--- src/Gujaratipages/SchemesGuj.js
+++ src/Gujaratipages/SchemesGuj.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function SchemesGuj() {
-  return <div>Schemes gujarati</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/test")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <>
+      <div>Schemes gujarati</div>
+      <div>
+        {data ? (
+          <div>
+            <p>Name: {data.name}</p>
+            <p>Email: {data.email}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
 }
